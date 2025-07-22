8d41535e38eba0df816f40041dd642dba6cc2ff5
lucapagliari <l.pagliari365@outlook.it>
2025-07-22 20:55:26 +0000

    add verbose mode

--- main.py
+++ main.py
@@ -2,27 +2,37 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
     if not args:
-        print("Error, prompt is missing")
+        print("Usage: python main.py \"your prompt here\" [--verbose]")
         sys.exit(1)
+
     user_prompt = " ".join(args)
     
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
     
     response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents=user_prompt
+    contents=messages
     )
     
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
